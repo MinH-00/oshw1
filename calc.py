74bf6baca41abf15f1d45d828518d98d57edb564
ymh0977 <144010663+ymh0977@naver.com>
2024-10-04 13:02:25 +0900

    2nd: 곱셈,나눗셈 기능 추가

--- calc.py
+++ calc.py
@@ -1,11 +1,11 @@
-# 덧셈과 뺄셈 프로그램
-def add_and_subtract(a, b):
-    return a + b, a - b
+# 덧셈, 뺄셈, 곱셈, 나눗셈 프로그램
+def add_subtract_multiply_divide(a, b):
+    return a + b, a - b, a * b, a / b if b != 0 else "0으로 나눌 수 없습니다"
 
 if __name__ == "__main__":
     a = int(input("첫 번째 숫자를 입력하세요: "))
     b = int(input("두 번째 숫자를 입력하세요: "))
     
-    result_sum, result_diff = add_and_subtract(a, b)
+    result_sum, result_diff, result_mul, result_div = add_subtract_multiply_divide(a, b)
     
-    print(f"합: {result_sum}, 차: {result_diff}")
+    print(f"합: {result_sum}, 차: {result_diff}, 곱: {result_mul}, 나눗셈: {result_div}")
